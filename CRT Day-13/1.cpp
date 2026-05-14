dedda8703cc5df0c4ec1d619621df2976ac31a5c
JatinSingh8003 <jatinpalsingh8003@gmail.com>
2026-05-14 08:55:58 +0530

    Add files via upload

---
+++ CRT Day-13/1.cpp
@@ -0,0 +1,20 @@
+// sum of digits
+
+#include<iostream>
+using namespace std;
+
+int sumofDigits(int x)
+{
+    //Base Case
+    if(x==0)
+        return 0;
+    return (x % 10) + sumofDigits(x / 10);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "Sum of Digits of " << n << " is : " << sumofDigits(n) << endl;
+    return 0;
+}
---
+++ CRT Day-13/3.cpp
@@ -0,0 +1,34 @@
+// linked list
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node(int x){
+            data=x;
+            next=NULL;
+        }
+};
+
+void printNodes(Node* head)
+{
+    Node* temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;    
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Node* head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    printNodes(head);
+    return 0;
+}
