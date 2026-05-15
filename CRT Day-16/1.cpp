fa5fd655b89a0f14ba3dc47cad04b755fefe15af
JatinSingh8003 <jatinpalsingh8003@gmail.com>
2026-05-15 09:42:44 +0530

    Add files via upload

---
+++ CRT Day-16/1.cpp
@@ -0,0 +1,21 @@
+// make a stack of plates, where if the stack gets too high, it starts a new stack
+
+#include<iostream>
+using namespace std;
+
+class stack {
+    private:
+        Node * head;
+        int max_size;
+        int current_size;
+        stack(int x) {
+            max_size=x;
+            current_size=0;
+            head=NULL;
+        }
+    void push(int x) {
+        
+    }
+
+         
+}
